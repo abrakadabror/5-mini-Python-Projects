a0186682e5f9e6791deb53c3b80e41a415d84825
Kamil <77681807+abrakadabror@users.noreply.github.com>
2023-07-06 16:01:22 +0200

    dodano lower aby nie wystąpił bład związany z wielkoscią liter

--- quiz_game.py
+++ quiz_game.py
@@ -2,24 +2,27 @@ print('Welcome to my computer quiz!')
 
 playing = input('Do you want to play?')
 
-if playing != str('yes'):
+text = 'Kamil IS GREAT!'
+print(text.lower())
+
+if playing.lower() != str('yes'):
     print('User refused to continue')
     quit()
 print("Okay! Let's play:)")
 
 answer = input('What does CPD stand for? ')
-if answer == 'counting digits':
+if answer.lower == 'counting digits':
     print('Thats correct answer')
 else:
     print('thats wrong answer')
 
-answer = input('What is wrong with you ')
+answer = input('What is wrong with you ').lower
 if answer == 'everything is fine':
     print('Thats correct answer')
 else:
     print('thats wrong answer')
 
-answer = input('Why are you sleeping? ')
+answer = input('Why are you sleeping? ').lower
 if answer == 'im not sleeping':
     print('Thats correct answer')
 else:
