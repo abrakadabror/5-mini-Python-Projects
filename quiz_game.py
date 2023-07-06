021833214f97b2cb05414dc3d441fe16c2da4464
Kamil <77681807+abrakadabror@users.noreply.github.com>
2023-07-06 15:43:54 +0200

    dodano linijkę else

--- quiz_game.py
+++ quiz_game.py
@@ -3,6 +3,12 @@ print('Welcome to my computer quiz!')
 playing = input('Do you want to play?')
 
 if playing != str('yes'):
-    print('user refused to continue')
+    print('User refused to continue')
     quit()
+print("Okay! Let's play:)")
 
+answer = input('What does CPD stand for? ')
+if answer == 'counting digits' or 'Counting Digits':
+    print('Thats correct answer')
+else:
+    print('thats wrong answer')
