8a1e781d79687e41fb29b7c85a38de9defbc8ad1
Kamil <77681807+abrakadabror@users.noreply.github.com>
2023-07-06 15:51:15 +0200

    Dodano kolejne pytania do quizu

--- quiz_game.py
+++ quiz_game.py
@@ -8,7 +8,19 @@ if playing != str('yes'):
 print("Okay! Let's play:)")
 
 answer = input('What does CPD stand for? ')
-if answer == 'counting digits' or 'Counting Digits':
+if answer == 'counting digits':
+    print('Thats correct answer')
+else:
+    print('thats wrong answer')
+
+answer = input('What is wrong with you ')
+if answer == 'everything is fine':
+    print('Thats correct answer')
+else:
+    print('thats wrong answer')
+
+answer = input('Why are you sleeping? ')
+if answer == 'im not sleeping':
     print('Thats correct answer')
 else:
     print('thats wrong answer')
