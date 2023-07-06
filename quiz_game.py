abd910b6ed94f710fe7adb86677710a606e32807
Kamil <77681807+abrakadabror@users.noreply.github.com>
2023-07-06 16:11:42 +0200

    dodano licznik poprawnych odpowiedzi

--- quiz_game.py
+++ quiz_game.py
@@ -9,21 +9,27 @@ if playing.lower() != str('yes'):
     print('User refused to continue')
     quit()
 print("Okay! Let's play:)")
-
+score = 0
 answer = input('What does CPD stand for? ')
 if answer.lower == 'counting digits':
     print('Thats correct answer')
+    score += 1
+    
 else:
     print('thats wrong answer')
 
 answer = input('What is wrong with you ').lower
 if answer == 'everything is fine':
     print('Thats correct answer')
+    score += 1
 else:
     print('thats wrong answer')
 
 answer = input('Why are you sleeping? ').lower
 if answer == 'im not sleeping':
     print('Thats correct answer')
+    score += 1
 else:
     print('thats wrong answer')
+
+print('You got' + str(score) + 'question correct' )
