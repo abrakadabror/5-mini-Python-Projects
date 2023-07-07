0d7d79d80efe70970f9e8d89834b5cbbc4c9c04e
Kamil <77681807+abrakadabror@users.noreply.github.com>
2023-07-07 12:34:58 +0200

    the final version

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -18,15 +18,19 @@ while True:
 
     if user_input == 'rock' and computer_pick == 'scissors': #sprawdzamy czy warunek po lewej stronie jest rowny warunkowi po prawej, jest oba sa rowne to wtedy jest ok
         print('You won!') #wyswietli you won jesli oba warunki sa rowne sobie 
-        user_wins += 1
-        continue
-    if user_input == 'paper' and computer_pick == 'rock': #sprawdzamy czy warunek po lewej stronie jest rowny warunkowi po prawej, jest oba sa rowne to wtedy jest ok
+        user_wins += 1 #dodaje ilosc wygranych usera
+    elif user_input == 'paper' and computer_pick == 'rock': #sprawdzamy czy warunek po lewej stronie jest rowny warunkowi po prawej, jest oba sa rowne to wtedy jest ok
         print('You won!') #wyswietli you won jesli oba warunki sa rowne sobie 
         user_wins += 1
-        continue
-    if user_input == 'scissors' and computer_pick == 'paper': #sprawdzamy czy warunek po lewej stronie jest rowny warunkowi po prawej, jest oba sa rowne to wtedy jest ok
+    elif user_input == 'scissors' and computer_pick == 'paper': #sprawdzamy czy warunek po lewej stronie jest rowny warunkowi po prawej, jest oba sa rowne to wtedy jest ok
         print('You won!') #wyswietli you won jesli oba warunki sa rowne sobie 
         user_wins += 1
-        continue
+    else: 
+        print('You lost')
+        computer_wins += 1 #zlicza ilosc wygranych komputera
+        
 print('Goodbye!')
+print(f'You won: {user_wins}')
+print('You won', user_wins, 'times.')
+print('The computer won', computer_wins, 'times')
 
