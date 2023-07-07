611c51fa9479c9c210e94a13c199cb08a0656ec1
Kamil <77681807+abrakadabror@users.noreply.github.com>
2023-07-07 10:39:21 +0200

    dodanie linijek print - mowiacych o tym w ilu probach udalo sie zgadnac uzytkownikowi numer

--- number_gueesser.py
+++ number_gueesser.py
@@ -11,7 +11,7 @@ import random #domyslnie w pythonie
 
 
 top_of_range =  input('Type a number: ') #uzytkownik podaje numer
-
+guesses = 0
 if top_of_range.isdigit():
     top_of_range = int(top_of_range)
     if top_of_range <0:  #podana liczba przez uzytkownika musi byc wieksza od 0
@@ -24,9 +24,18 @@ random_number = random.randint(0, top_of_range) #uzywamy top_of_range do generow
 print(random_number) #drukujemy random_number z lini wyzej
 
 while True:
+    guesses += 1 #bedziemy zliaczac w tym miejsu ilosc prob uzytkownika potrzebnych do zgadniencia liczby
     user_guess = input('Make a guess: ')
     if user_guess.isdigit():
         user_guess = int(user_guess)
     else:
         print('Please type a number next time.') #jesli nie jest wyswietl ten komunikat
-        continue
+        continue #sprowadza nas ponownie na start petli while
+    if user_guess == random_number:
+        print('You got it!')
+        break #przerywamy break petle while
+    else:
+        print('You got it wrong') #zeby przerwac petle nieskonczona nalezy wcisnac crtl + c
+
+print(f'You got it in:  {guesses}  Congratulations!') #sprawdzamy z f-stringiem w w ilu probach udalo sie zgadnac odpowiedz uztykownikowi
+print('You got it', guesses, 'attempts') #sprawdzamy w ilu probach udalo sie zgadnac odpowiedz uztykownikowi
