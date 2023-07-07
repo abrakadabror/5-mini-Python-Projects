42b9a5134908779102269107913613189822a14e
Kamil <77681807+abrakadabror@users.noreply.github.com>
2023-07-07 16:18:24 +0200

    dodano kolejne linijki kodu

--- choose_your_own_adventure.py
+++ choose_your_own_adventure.py
@@ -5,12 +5,18 @@ answer = input('You are on a dirty road, it has come to an end and you can go le
 if answer == 'left': #jesli odpowiedz to w lewo 
     answer = input("You come to a river, you can walk around or swim across? Type walk to walk around and swim to siwm accreoss") # to zadajemy pytanie
     if answer == 'swim': #sprawdzamy w if jaka jest odpowiedz na powyzsze pytanie,  jesli odpowiedz to swim to nic nie "wydrukuje"
-        print()
+        print(' You swam across and were eaten by aligator.')
     elif answer =='walk': #jesli odpowiedz to walk 
-        print()
+        print('You walked for many miles, ran out of water and you lose a game')
     else:
         print('Not a valid option.You lose.')
 elif answer == 'right':
-    print()
+    answer = input("You come to a bridge. It's looks woobly, do you want to cross it?")
+    if answer == 'back':
+         print('You were kicked in the ass by a human-tourch')
+    elif answer =='cross':
+        print('You walked for many miles, ran out of water and you lost the game')
+    else: 
+        print('Not a valid option. You lose.')
 else:
     print('Not a valid option.You lose.')
