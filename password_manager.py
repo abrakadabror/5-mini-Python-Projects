cd25a0e0be0f15891413ff8db1b8b15c1e34f4df
Kamil <77681807+abrakadabror@users.noreply.github.com>
2023-07-09 08:43:52 +0200

    first steps

---
+++ password_manager.py
@@ -0,0 +1,21 @@
+#manadzer hasel
+pwd = input('what is the master passowrd?')
+
+
+def view():
+    pass
+def add():
+    pass
+
+
+while True: # gdy prawdziwe to
+    mode = input('Wodul you like to add a new passowrd or view a existing ones(view, add, press q to quit)?')
+    if mode = 'q': #przerywamy dzialanie programu
+        break
+    if mode = 'view': # jesli uzytkownik poda view
+        view() #wywolujemy funckje view()
+    elif mode == 'add': #jesli uzytkownik poda add
+        add() #wywolujemy funkcje add()
+    else: #jesli nie poda zadnego z powyzszych
+        print('Invalid mode.')
+        continue #wracamy na poczatek petli
